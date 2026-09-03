5c785ca152e2ffcce351f094b3af17c0071db109
AgnesLovelyn <agneslovelyn60@gmail.com>
2026-09-03 07:12:41 +0700

    package lock json dan controller outfit

--- src/outfit/outfit.controller.ts
+++ src/outfit/outfit.controller.ts
@@ -7,16 +7,22 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { OutfitService } from './outfit.service';
 import { CreateOutfitDto } from './dto/create-outfit.dto';
 import { UpdateOutfitDto } from './dto/update-outfit.dto';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { RolesGuard } from '../common/guards/roles.guard';
+import { Roles } from '../common/decorators/roles.decorator';
 
 @Controller('outfits')
 export class OutfitController {
   constructor(private readonly outfitService: OutfitService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   create(@Body() createOutfitDto: CreateOutfitDto) {
     return this.outfitService.create(createOutfitDto);
   }
@@ -32,6 +38,8 @@ export class OutfitController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateOutfitDto: UpdateOutfitDto,
@@ -40,6 +48,8 @@ export class OutfitController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.outfitService.remove(id);
   }
